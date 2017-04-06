bb6c4124aa442ea8c00dd21375a4a24d959e07c5
eliafornari <fornari.elia@gmail.com>
2017-04-06 02:06:43 -0700

    added project element

--- server/main.js
+++ server/main.js
@@ -13,13 +13,13 @@ let prismic = require('./api/prismic');
 
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
-// app.use(function(req, res, next) {
-//     if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-//         res.redirect('https://' + req.get('Host') + req.url);
-//     }
-//     else
-//         next();
-// });
+app.use(function(req, res, next) {
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+    else
+        next();
+});
 app.set('views', __dirname + '/../client');
 app.use( express.static(__dirname + "/../client") );
 app.use(bodyParser.json()); // support json encoded bodies
